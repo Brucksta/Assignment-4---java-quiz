a9509c366e9f191ef76a5d887a14d6712b369779
Peter Bruckner <peterbruckner08@gmail.com>
2021-03-23 18:29:26 +1030

    set some questions

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var question1 = {
+    question:"Which coding language is in charge of making a website look good?";
+    answer1:"CSS"
+    answer2:"HTML"
+    answer3:"Javascript"
+    answer4:"Python"
+}
+
+var question2 = {
+    question:"",
+    answer1:"",
+    answer2:"",
+    answer3:"",
+    answer4:"",
+}
+
+var question3 = {
+
+}
+
+var question4 = {
+
+}
