e635f91922cfb75be09fd0781eb66a0bd2dca6b9
Peter Bruckner <peterbruckner08@gmail.com>
2021-03-24 13:42:29 +1030

    setup timer and position timer

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var question1 = {
-    question:"Which coding language is in charge of making a website look good?";
-    answer1:"CSS"
-    answer2:"HTML"
-    answer3:"Javascript"
-    answer4:"Python"
+    question:"Which coding language is in charge of making a website look good?",
+    answer1:"CSS",
+    answer2:"HTML",
+    answer3:"Javascript",
+    answer4:"Python",
 }
 
 var question2 = {
@@ -21,3 +21,23 @@ var question3 = {
 var question4 = {
 
 }
+var timeEl = document.querySelector(".time");
+var secondsLeft = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left";
+  
+      if(secondsLeft === 0) {
+
+        clearInterval(timerInterval);
+
+      }
+  
+    }, 1000);
+}
+
+
+setTime()
