a60f917fab70c7484f7a41f422e4c33684478be6
Peter Bruckner <peterbruckner08@gmail.com>
2021-03-29 18:04:02 +1030

    reformat and call questions

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,14 @@
 const startTimer = document.getElementById('start-btn')
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+let shuffledQuestions
+let currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
 var timeEl = document.querySelector('.time')
-var question1 = {
-    question:"Which coding language is in charge of making a website look good?",
-    answer1:"CSS",
-    answer2:"HTML",
-    answer3:"Javascript",
-    answer4:"Python",
-}
-
-var question2 = {
-    question:"",
-    answer1:"",
-    answer2:"",
-    answer3:"",
-    answer4:"",
-}
-
-var question3 = {
-
-}
-
-var question4 = {
-
-}
-
+]
 var secondsLeft = 60;
-
 
 function setTime() {
     // Sets interval in variable
@@ -44,9 +27,46 @@ function setTime() {
 
 startTimer.addEventListener("click", setTime);
 
-const startButton = document.getElementById('start-btn')
+
+//starting game
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-console.log('started')
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+function showQuestion(question) {
+  questionElement.innerText = question.question
+}
+
+var questions = [
+  { question:"Which coding language is in charge of making a website look good?",
+  answers: [
+    { text: "CSS",
+  answer2:"HTML",
+  answer3:"Javascript",
+  answer4:"Python",
+}
+
+var question2 = {
+  question:"",
+  answer1:"",
+  answer2:"",
+  answer3:"",
+  answer4:"",
+}
+
+var question3 = {
+
+}
+
+var question4 = {
+
 }
